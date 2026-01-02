c470046d3f127550a5ce0999b59f63c69828fd0d
Daniel <kingdanman213@gmail.com>
2026-01-02 10:53:09 -0500

    menu

--- app.js
+++ app.js
@@ -145,40 +145,288 @@ function parseIngredients(recipeText){
 // ===== Menu imported from your Excel (RECIPE sheet) =====
 // Each item has sizes (some missing), and we use the recipe text for shots/milk/ingredients.
 const MENU_ITEMS = [
-  {"name":"COLD BREW","abbrev":"CB","sizes":{"Iced 16":"8OZ COLD BREW + ICE","Iced 20":"10OZ COLD BREW + ICE"}},
-  {"name":"AMERICANO","abbrev":"A","sizes":{"Hot 12":"9OZ HOT WATER + 2 SHOTS","Hot 16":"13OZ HOT WATER + 2 SHOTS","Iced 16":"8OZ WATER + 2 SHOTS + ICE","Iced 20":"10OZ WATER + 3 SHOTS + ICE"}},
-  {"name":"CAPPUCCINO**","abbrev":"CAP","sizes":{"Hot 12":"2 SHOTS + STEAMED FOAM MILK","Hot 16":"2 SHOTS + STEAMED FOAM MILK"}},
-  {"name":"LATTE (FLAT WHITE)","abbrev":"L (FW)","sizes":{"Hot 12":"2 SHOTS + STEAMED MILK","Hot 16":"2 SHOTS + STEAMED MILK","Iced 16":"8OZ MILK + 2 SHOTS + ICE","Iced 20":"10OZ MILK + 3 SHOTS + ICE"}},
-  {"name":"MOCHA LATTE","abbrev":"ML","sizes":{"Hot 12":"2 M SAUCE + 2 SHOTS + STEAMED MILK + WC?","Hot 16":"2.5 M SAUCE + 2 SHOTS + STEAMED MILK + WC?","Iced 16":"2 M SAUCE + 8OZ MILK + 2 SHOTS + ICE + WC?","Iced 20":"2.5 M SAUCE + 10OZ MILK + 3 SHOTS + ICE + WC?"}},
-  {"name":"WHITE MOCHA LATTE","abbrev":"WML","sizes":{"Hot 12":"2 WM + 2 SHOTS + STEAMED MILK + WC?","Hot 16":"2.5 WM + 2 SHOTS + STEAMED MILK + WC?","Iced 16":"2 WM + 8OZ MILK + 2 SHOTS + ICE + WC?","Iced 20":"2.5 WM + 10OZ MILK + 3 SHOTS + ICE + WC?"}},
-  {"name":"CARAMEL LATTE","abbrev":"CL","sizes":{"Hot 12":"2 CARAMEL SAUCE + 2 SHOTS + STEAMED MILK + WC?","Hot 16":"2.5 CARAMEL SAUCE + 2 SHOTS + STEAMED MILK + WC?","Iced 16":"2 CARAMEL SAUCE + 8OZ MILK + 2 SHOTS + ICE + WC?","Iced 20":"2.5 CARAMEL SAUCE + 10OZ MILK + 3 SHOTS + ICE + WC?"}},
-  {"name":"VANILLA LATTE","abbrev":"VL","sizes":{"Hot 12":"2.5 VANILLA SYRUP + 2 SHOTS + STEAMED MILK","Hot 16":"3 VANILLA SYRUP + 2 SHOTS + STEAMED MILK","Iced 16":"2.5 VANILLA SYRUP + 8OZ MILK + 2 SHOTS + ICE","Iced 20":"3.5 VANILLA SYRUP + 10OZ MILK + 3 SHOTS + ICE"}},
-  {"name":"LAVENDER LATTE","abbrev":"LL","sizes":{"Hot 12":"3 LAV + 1.5 SIMPLE + 2 SHOTS + STEAMED MILK","Hot 16":"4 LAV + 2 SIMPLE + 2 SHOTS + STEAMED MILK","Iced 16":"3 LAV + 1.5 SIMPLE + 8OZ MILK + 2 SHOTS + ICE","Iced 20":"3 LAV + 1.5 SIMPLE + 10OZ MILK + 3 SHOTS + ICE"}},
-  {"name":"ROSE LATTE","abbrev":"RL","sizes":{"Hot 12":"3 ROSE + 1.5 SIMPLE + 2 SHOTS + STEAMED MILK","Hot 16":"4 ROSE + 2 SIMPLE + 2 SHOTS + STEAMED MILK","Iced 16":"3 ROSE + 1.5 SIMPLE + 8OZ MILK + 2 SHOTS + ICE","Iced 20":"3 ROSE + 1.5 SIMPLE + 10OZ MILK + 3 SHOTS + ICE"}},
-  {"name":"HONEY CINNAMON LATTE","abbrev":"HCL","sizes":{"Hot 12":"2.5 HONEY + 2 SHOTS + STEAMED MILK + CINN","Hot 16":"3 HONEY + 2 SHOTS + STEAMED MILK + CINN","Iced 16":"2.5 HONEY + 8OZ MILK + 2 SHOTS + ICE + CINN","Iced 20":"3.5 HONEY + 10OZ MILK + 3 SHOTS + ICE + CINN"}},
+  {
+    
+  },
+  {
+    "name": "COLD BREW",
+    "abbrev": "CB",
+    "sizes": {
+      "Iced 16": "8OZ COLD BREW + ICE",
+      "Iced 20": "10OZ COLD BREW + ICE"
+    }
+  },
+  {
+    "name": "AMERICANO",
+    "abbrev": "A",
+    "sizes": {
+      "Hot 12": "9OZ HOT WATER + 2 SHOTS",
+      "Hot 16": "13OZ HOT WATER + 2 SHOTS",
+      "Iced 16": "8OZ WATER + 2 SHOTS + ICE",
+      "Iced 20": "10OZ WATER + 3 SHOTS + ICE"
+    }
+  },
+  {
+    "name": "CAPPUCCINO**",
+    "abbrev": "CAP",
+    "sizes": {
+      "Hot 12": "2 SHOTS + STEAMED FOAM MILK",
+      "Hot 16": "2 SHOTS + STEAMED FOAM MILK"
+    }
+  },
+  {
+    "name": "LATTE (FLAT WHITE)",
+    "abbrev": "L (FW)",
+    "sizes": {
+      "Hot 12": "2 SHOTS + STEAMED MILK",
+      "Hot 16": "2 SHOTS + STEAMED MILK",
+      "Iced 16": "8OZ MILK + 2 SHOTS + ICE",
+      "Iced 20": "10OZ MILK + 3 SHOTS + ICE"
+    }
+  },
+  
+  {
+    "name": "MOCHA",
+    "abbrev": "M",
+    "sizes": {
+      "Hot 12": "2 M SAUCE + 2 SHOTS + STEAMED MILK + WC?",
+      "Hot 16": "2.5 M SAUCE + 2 SHOTS + STEAMED MILK + WC?",
+      "Iced 16": "2 M SAUCE + 2 SHOTS + 8OZ MILK + ICE",
+      "Iced 20": "2.5 M SAUCE + 3 SHOTS + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "WHITE MOCHA",
+    "abbrev": "WM",
+    "sizes": {
+      "Hot 12": "2 WM SAUCE + 2 SHOTS + STEAMED MILK + WC?",
+      "Hot 16": "2.5 WM SAUCE + 2 SHOTS + STEAMED MILK + WC?",
+      "Iced 16": "2 WM SAUCE + 2 SHOTS + 8OZ MILK + ICE",
+      "Iced 20": "2.5 WM SAUCE + 3 SHOTS + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "CARAMEL LATTE",
+    "abbrev": "CAR L",
+    "sizes": {
+      "Hot 12": "1.5 CAR SAUCE + 2 SHOTS + STEAMED MILK",
+      "Hot 16": "2 CAR SAUCE + 2 SHOTS + STEAMED MILK",
+      "Iced 16": "2 CAR SAUCE + 2 SHOTS + 8OZ MILK + ICE",
+      "Iced 20": "2.5 CAR SAUCE  + 3 SHOTS + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "CARAMEL MACCHIATO",
+    "abbrev": "CM",
+    "sizes": {
+      "Hot 12": "1.5 CAR SAUCE + 0.5 V SYRUP + 2 SHOTS + STEAMED MILK",
+      "Hot 16": "2 CAR SAUCE + 0.5 V + 2 SHOTS + STEAMED MILK",
+      "Iced 16": "2 CAR SAUCE + 0.5 V SYRUP + 2 SHOTS + 8OZ MILK + ICE",
+      "Iced 20": "2.5 CAR SAUCE + 0.5 V SYRUP + 3 SHOTS + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "DAVANT COFFEE",
+    "abbrev": "DV",
+    "sizes": {
+      "Hot 12": "4 SIMPLE + 2 SHOTS + STEAMED BREVE ( half and half)",
+      "Hot 16": "5 SIMPLE + 2 SHOTS + STEAMED BREVE",
+      "Iced 16": "4 SIMPLE + 2 SHOTS + 8OZ BREVE + ICE",
+      "Iced 20": "5 SIMPLE + 3 SHOTS + 10OZ BREVE + ICE"
+    }
+  },
+  {
+    "name": "HONEY CINNAMON LATTE",
+    "abbrev": "HCL",
+    "sizes": {
+      "Hot 12": "2.5 HONEY + CINN + 2 SHOTS + STEAMED MILK",
+      "Hot 16": "3.5 HONEY + CINN + 2 SHOTS + STEAMED MILK",
+      "Iced 16": "2.5 HONEY + CINN + 2 SHOTS + 8OZ MILK + ICE",
+      "Iced 20": "3.5 HONEY + CINN + 3 SHOTS + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "ROSE LATTE",
+    "abbrev": "RL",
+    "sizes": {
+      "Hot 12": "3 ROSE + 1.5 SIMPLE + 2 SHOTS + STEAMED MILK",
+      "Hot 16": "4 ROSE + 2 SIMPLE + 2 SHOTS + STEAMED MILK",
+      "Iced 16": "3 ROSE + 1.5 SIMPLE + 2 SHOTS + 8OZ MILK + ICE",
+      "Iced 20": "3 ROSE + 1.5 SIMPLE + 3 SHOTS + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "LAVENDER LATTE",
+    "abbrev": "LL",
+    "sizes": {
+      "Hot 12": "3 LAV + 1.5 SIMPLE + 2 SHOTS + STEAMED MILK",
+      "Hot 16": "4 LAV + 2 SIMPLE + 2 SHOTS + STEAMED MILK",
+      "Iced 16": "3 LAV + 1.5 SIMPLE + 2 SHOTS + 8OZ MILK + ICE",
+      "Iced 20": "3 LAV + 1.5 SIMPLE + 3 SHOTS + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "CHAI LATTE",
+    "abbrev": "CHAI",
+    "sizes": {
+      "Hot 12": "90ml CHAI + 180ml MILK",
+      "Hot 16": "120ml CHAI + 230ml MILK",
+      "Iced 16": "90ml CHAI + 180ml MILK + ICE + CINN",
+      "Iced 20": "120ml CHAI + 230ml MILK  MILK + ICE + CINN"
+    }
+  },
+  {
+    "name": "MATCHA LATTE",
+    "abbrev": "MT",
+    "sizes": {
+      "Hot 12": "2 SIMPLE + 30g MATCHA MIX + 9OZ MILK",
+      "Hot 16": "2.5 SIMPLE + 30g MATCHA MIX + 13OZ MILK",
+      "Iced 16": "2 SIMPLE + 30g MATCHA MIX + 9OZ MILK + ICE",
+      "Iced 20": "2.5 SIMPLE + 30g MATCHA MIX + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "LAVENDER MATCHA LATTE",
+    "abbrev": "LMT",
+    "sizes": {
+      "Hot 12": "2 SIMPLE + 2 LAV + 30g MATCHA MIX + 9OZ MILK",
+      "Hot 16": "2.5 SIMPLE + 2.5 LAV + 30g MATCHA MIX + 13OZ MILK",
+      "Iced 16": "2 SIMPLE + 2 LAV + 30g MATCHA MIX + 9OZ MILK + ICE",
+      "Iced 20": "2.5 LAV + 30g MATCHA MIX + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "WHITE MATCHA LATTE",
+    "abbrev": "WMT",
+    "sizes": {
+      "Hot 12": "2 SIMPLE + 1 WM + 30g MATCHA MIX + 9OZ MILK",
+      "Hot 16": "2.5 SIMPLE + 1.5 WM + 30g MATCHA MIX + 13OZ MILK",
+      "Iced 16": "2 SIMPLE + 1 WM + 30g MATCHA MIX + 9OZ MILK + ICE",
+      "Iced 20": "1.5 WM + 30g MATCHA MIX + 10OZ MILK + ICE"
+    }
+  },
+  {
+    "name": "HOJICHA / EARLGREY LATTE",
+    "abbrev": "HJ / EG",
+    "sizes": {
+      "Hot 12": "2 SIMPLE + 60g POWDER MIX + 9OZ MILK",
+      "Hot 16": "2.5 SIMPLE + 65g POWDER MIX + 13OZ MILK",
+      "Iced 16": "2 SIMPLE + 60g POWDER MIX + 9OZ MILK + ICE",
+      "Iced 20": "10OZ MILK + 65g POWDER MIX + ICE"
+    }
+  },
+  {
+    "name": "HOT/ICED CHOCOLATE",
+    "abbrev": "HC / CHOCO",
+    "sizes": {
+      "Hot 12": "9OZ MILK + 2.5 M SAUCE + STIR + (WC) + COCOA",
+      "Hot 16": "13OZ MILK + 3.5 M SAUCE + STIR + (WC) + COCOA",
+      "Iced 16": "9OZ MILK + 2.5 M SAUCE + STIR + ICE + (WC) + COCOA",
+      "Iced 20": "13OZ MILK + 3.5 M SAUCE + STIR + ICE + (WC) + COCOA"
+    }
+  },
+  {
+    "name": "PUMPKIN CINNAMON LATTE",
+    "abbrev": "PCL",
+    "sizes": {
+      "Hot 12": "2 PUMPKIN SAUCE + CINNAMON(P) + 2 SHOTS + STEAMED MILK + WC",
+      "Hot 16": "2.5 PUMPKIN SAUCE + CINNAMON(P) + 2 SHOTS + STEAMED MILK + WC",
+      "Iced 16": "2.5 PUMPKIN SAUCE + CINNAMON(P) + 2 SHOTS + 8OZ MILK + ICE + WC",
+      "Iced 20": "3 PUMPKIN SAUCE + CINNAMON(P) + 3 SHOTS + 10OZ MILK + ICE + WC"
+    }
+  },
+  {
+    "name": "WHITE LAVENDER LATTE",
+    "abbrev": "WL",
+    "sizes": {
+      "Hot 12": "1 WM + 1.5 LAV + 2 SHOTS +STEAMED MILK",
+      "Hot 16": "1.5 WM + 2 LAV + 2 SHOTS +STEAMED MILK",
+      "Iced 16": "1 WM + 1.5 LAV + 2 SHOTS + 8OZ MILK + ICE",
+      "Iced 20": "1.5 WM + 2 LAV + 3 SHOTS + 10 OZ MILK + ICE"
+    }
+  },
 
-  {"name":"MATCHA LATTE","abbrev":"MT","sizes":{"Hot 12":"9OZ MILK + 60g POWDER MIX + 2 SHOTS? (IF REQUESTED)","Hot 16":"13OZ MILK + 65g POWDER MIX","Iced 16":"9OZ MILK + 60g POWDER MIX + ICE","Iced 20":"10OZ MILK + 65g POWDER MIX + ICE"}},
-  {"name":"HOJICHA / EARLGREY LATTE","abbrev":"HJ / EG","sizes":{"Hot 12":"9OZ MILK + 60g POWDER MIX","Hot 16":"13OZ MILK + 65g POWDER MIX","Iced 16":"9OZ MILK + 60g POWDER MIX + ICE","Iced 20":"10OZ MILK + 65g POWDER MIX + ICE"}},
-  {"name":"HOT/ICED CHOCOLATE","abbrev":"HC / CHOCO","sizes":{"Hot 12":"9OZ MILK + 2.5 M SAUCE + STIR + (WC) + COCOA","Hot 16":"13OZ MILK + 3.5 M SAUCE + STIR + (WC) + COCOA","Iced 16":"9OZ MILK + 2.5 M SAUCE + STIR + ICE + (WC) + COCOA","Iced 20":"13OZ MILK + 3.5 M SAUCE + STIR + ICE + (WC) + COCOA"}},
-  {"name":"PUMPKIN CINNAMON LATTE","abbrev":"PCL","sizes":{"Hot 12":"2 PUMPKIN SAUCE + CINNAMON(P) + 2 SHOTS + STEAMED MILK + WC","Hot 16":"2.5 PUMPKIN SAUCE + CINNAMON(P) + 2 SHOTS + STEAMED MILK + WC","Iced 16":"2.5 PUMPKIN SAUCE + CINNAMON(P) + 2 SHOTS + 8OZ MILK + ICE + WC","Iced 20":"3 PUMPKIN SAUCE + CINNAMON(P) + 3 SHOTS + 10OZ MILK + ICE + WC"}},
-  {"name":"WHITE LAVENDER LATTE","abbrev":"WL","sizes":{"Hot 12":"1 WM + 1.5 LAV + 2 SHOTS +STEAMED MILK","Hot 16":"1.5 WM + 2 LAV + 2 SHOTS +STEAMED MILK","Iced 16":"1 WM + 1.5 LAV + 2 SHOTS + 8OZ MILK + ICE","Iced 20":"1.5 WM + 2 LAV + 3 SHOTS + 10 OZ MILK + ICE"}},
-
-  {"name":"CREAM COFFEE","abbrev":"CC","sizes":{"Iced 16":"2.5 SIMPLE + 6OZ COLDBREW +ICE + CREAM ON TOP \n(CREAM 1.5 SIMPLE 1.5 OZ + HEAVY CREAM) *2 PORTIONS USE 1/2","Iced 20":"3 SIMPLE + 8OZ COLDBREW +ICE + CREAM ON TOP \n(CREAM 1.5 SIMPLE 1.5 OZ + HEAVY CREAM) *2 PORTIONS USE 1/2"}},
-  {"name":"PUMPKIN FLOAT","abbrev":"PF","sizes":{"Iced 16":"1.5 SIMPLE + 6OZ COLDBREW +ICE + CREAM ON TOP w/CINN POWDER\nPUMPKIN CREAM ( 50ml HEAVY CREAM + 1.5 PUMPKIN)","Iced 20":"2 SIMPLE + 8OZ COLDBREW +ICE + CREAM ON TOP w/CINN POWDER\nPUMPKIN CREAM ( 50ml HEAVY CREAM + 1.5 PUMPKIN)"}},
-  {"name":"BUTTERSCOTCH LATTE","abbrev":"BSL","sizes":{"Iced 16":"1.5 SIMPLE + 2 SHOTS + 5OZ MILK + ICE + BS CREAM(25ML HEAVY CREAM + 50ML BS CREAM)TOTAL 75ML"}},
-  {"name":"ESPRESSODA","abbrev":"ESP","sizes":{"Iced 16":"4 VANILLA SYRUP + 2 SIMPLE + 10OZ CLUB SODA + ICE + SOLO ESP"}},
-
-  {"name":"GRAPEFRUIT ADE","abbrev":"GF ADE","sizes":{"Iced 16":"2 PUMPS SIMPLE + 3OZ GF JUICE + 1 SCOOP GRAPEFRUIT + ICE + CLUBSODA"}},
-  {"name":"COFFEE FRAP","abbrev":"COFFEE FRAP","sizes":{"Hot 12":"1.8 CUP ICE + 1 SCOOP FRAP POWDER + 5 SIMPLE\n1 SHOT + 4OZ COFFEE + BLEND+ WC?"}},
-  {"name":"SAUCE FRAP","abbrev":"M/WM/CAR FRAP","sizes":{"Hot 12":"1.8 CUP ICE + 1  SCOOP FRAP POWDER + 3 PUMPS SAUCE\n1 SHOT + 3OZ COFFEE + BLEND + WC?"}},
-  {"name":"GREEN TEA FRAP","abbrev":"GT FRAP","sizes":{"Hot 12":"1.5 CUP ICE + 2 SS + 2 SCOOPS GT POWDER + 9OZ MILK + BLEND + (WC)"}},
-
-  {"name":"(STRAW/BLUE) BERRY SMOOTHIE","abbrev":"SYS / BYS","sizes":{"Hot 12":"50% CUP OF FRUIT + 1.5 CUP OF ICE + 2 SIMPLE + (1 SCOOP BERRY POWDER 35-40g) YOGURT POWDER + 6OZ MILK + BLEND + (WC)"}},
-  {"name":"STRAWBERRY BANANA OAT MILK","abbrev":"SBO","sizes":{"Hot 12":"50% CUP OF STRAWBERRY + 1/2 BANANA + 1.5 CUP OF ICE + 4 HONEY \n8OZ OAT MILK + BLEND + (WC)"}},
+  // Cold/iced section (sheet columns represent Iced 16 / Iced 20)
+  {
+    "name": "CREAM COFFEE",
+    "abbrev": "CC",
+    "sizes": {
+      "Iced 16": "2.5 SIMPLE + 6OZ COLDBREW +ICE + CREAM ON TOP\nCREAM(1.5 ALM SYRUP +1.5 SIMPLE 1.5 OZ + HEAVY CREAM) *2 PORTIONS USE 1/2",
+      "Iced 20": "3 SIMPLE + 8OZ COLDBREW +ICE + CREAM ON TOP\nCREAM(1.5 ALM SYRUP +1.5 SIMPLE 1.5 OZ + HEAVY CREAM) *2 PORTIONS USE 1/2"
+    }
+  },
+  {
+    "name": "PUMPKIN FLOAT",
+    "abbrev": "PF",
+    "sizes": {
+      "Iced 16": "1.5 SIMPLE + 6OZ COLDBREW +ICE + CREAM ON TOP w/CINN POWDER\nPUMPKIN CREAM ( 50ml HEAVY CREAM + 1.5 PUMPKIN)",
+      "Iced 20": "3 SIMPLE + 8OZ COLDBREW +ICE + CREAM ON TOP w/CINN POWDER\nPUMPKIN CREAM ( 50ml HEAVY CREAM + 1.5 PUMPKIN)"
+    }
+  },
+  {
+    "name": "BUTTERSCOTCH LATTE",
+    "abbrev": "BSL",
+    "sizes": {
+      "Iced 16": "1.5 SIMPLE + 2 SHOTS + 5OZ MILK + ICE\nCREAM(25ML HEAVY CREAM + 50ML BS CREAM)TOTAL 75ML"
+    }
+  },
+  {
+    "name": "ESPRESSODA",
+    "abbrev": "ESP",
+    "sizes": {
+      "Iced 20": "4 VANILLA SYRUP + 2 SIMPLE + 10OZ CLUB SODA + ICE + SOLO ESP"
+    }
+  },
+  {
+    "name": "GRAPEFRUIT ADE",
+    "abbrev": "GF ADE",
+    "sizes": {
+      "Iced 20": "2 PUMPS SIMPLE + 3OZ GF JUICE + 1 SCOOP GRAPEFRUIT + ICE + CLUBSODA"
+    }
+  },
+  {
+    "name": "COFFEE FRAP",
+    "abbrev": "COFFEE FRAP",
+    "sizes": {
+      "Iced 16": "1.8 CUP ICE + 1 SCOOP FRAP POWDER + 4 SIMPLE\n1 SHOT + 3OZ COFFEE + BLEND + WC?",
+      "Iced 20": "1.8 CUP ICE + 1.2 SCOOP FRAP POWDER + 5 SIMPLE\n1 SHOT + 4OZ COFFEE + BLEND+ WC?"
+    }
+  },
+  {
+    "name": "SAUCE FRAP",
+    "abbrev": "M/WM/CAR FRAP",
+    "sizes": {
+      "Iced 16": "1.8 CUP ICE + 1  SCOOP FRAP POWDER + 3 PUMPS SAUCE\n1 SHOT + 3OZ COFFEE + BLEND + WC?",
+      "Iced 20": "1.8 CUP ICE + 1.2 SCOOP FRAP POWDER + 3.5 PUMPS SAUCE\n1 SHOT + 4OZ COFFEE + BLEND + WC?"
+    }
+  },
+  {
+    "name": "GREEN TEA FRAP",
+    "abbrev": "GT FRAP",
+    "sizes": {
+      "Iced 16": "1.5 CUP ICE + 2 SS + 2 SCOOPS GT  POWDER + 7OZ MILK + BLEND + (WC)",
+      "Iced 20": "1.5 CUP ICE + 2.5 SS + 2.5 SCOOPS GT POWDER + 9OZ MILK + BLEND + (WC)"
+    }
+  },
+  {
+    "name": "(STRAW/BLUE) BERRY SMOOTHIE",
+    "abbrev": "SYS / BYS",
+    "sizes": {
+      "Iced 16": "50% CUP OF FRUIT, 50% CUP OF ICE + 3 SIMPLE ( SYS) / 4 SIMPLE ( BYS)\n0.9 SCOOP(38-40g) YOGURT POWDER + 6OZ MILK + BLEND + (WC)"
+    }
+  },
+  {
+    "name": "STRAWBERRY BANANA OAT MILK",
+    "abbrev": "SBO",
+    "sizes": {
+      "Iced 16": "50% CUP OF STRAWBERRY 50% CUP OF ICE + 4 HONEY\n8OZ OAT MILK + BLEND + (WC)"
+    }
+  }
+];
 
   // Items listed higher on the sheet (espresso/macchiato/cortado/etc.) exist but were presented as service notes.
   // We can add them as orderable items cleanly in the next step if you want them randomized too.
-];
+;
 
 // ===== Order generation from full menu =====
 function randomOrder(){
